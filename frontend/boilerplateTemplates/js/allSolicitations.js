12a5352f4a82965c001804c13597c871e641c7c8
Rodrigo Savaris <rdsavaris@msn.com>
2019-08-09 06:43:43 +0100

    updating the windown

--- frontend/boilerplateTemplates/js/allSolicitations.js
+++ frontend/boilerplateTemplates/js/allSolicitations.js
@@ -11,11 +11,11 @@ $(document).ready(function () {
       var grid = '<div class="card" style="width: 30rem;">';
       grid+= '<img src="img/decoy.jpeg" class="card-img-top" alt="...">'
       grid+= '<div class="card-body">';
-      for (let sec = 0; sec < 4; sec++) {
-        
-        grid += '<h5 class="card-text">'+Object.values(response[index])[sec+1]+'</h5>';
-            
-      }
+     
+      grid += '<h5 class="card-text">'+"Solicitation identificator: "+response[index].id+'</h5>';
+      grid += '<h5 class="card-text">'+"Solicitator description: "+response[index].description+'</h5>';
+
+
       grid += '</div>';  
       grid += '<a href="#" class="btn btn-primary">View solicitation</a>';
 
@@ -33,17 +33,12 @@ $(document).ready(function () {
     
     // perform an ajax http get request
     $.ajax({
-        url: 'http://localhost:8080/projectx/api/person',
+        url: 'http://localhost:8080/projectx/api/person/listsolicitations',
         async: true,
         success: successCallback,
         error: errorCallback
     });
 
-
-
-
-
-    
       });
       function clickfun() {
       alert("OI");
